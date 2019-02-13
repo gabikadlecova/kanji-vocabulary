de69165fb894f65e5f8d6bafb22ed4a67c44d518
Gabriela Suchoparova <gabi.suchoparova@gmail.com>
2019-02-13 17:02:45 +0100

    Started writing training page

--- kanjivocabl/kanjiapp/trainwidget.cpp
+++ kanjivocabl/kanjiapp/trainwidget.cpp
@@ -6,9 +6,34 @@ TrainWidget::TrainWidget(QWidget *parent) :
     ui(new Ui::TrainWidget)
 {
     ui->setupUi(this);
+
+    setupLayout();
 }
 
 TrainWidget::~TrainWidget()
 {
     delete ui;
+}
+
+void TrainWidget::setTrainKanji(std::vector<kcomp> newTraining)
+{
+    trainKanji = std::move(newTraining);
+}
+
+void TrainWidget::setupLayout()
+{
+    l = new QGridLayout();
+
+    setupButtons();
+    setupTrainPage();
+}
+
+void TrainWidget::setupButtons()
+{
+    // TODO hide menu splitter in MainWidget!
+}
+
+void TrainWidget::setupTrainPage()
+{
+
 }
--- kanjivocabl/kanjiapp/trainwidget.h
+++ kanjivocabl/kanjiapp/trainwidget.h
@@ -2,6 +2,10 @@
 #define TRAINWIDGET_H
 
 #include <QWidget>
+#include <QGridLayout>
+#include <vector>
+
+#include "kanjiapp/KanjiData.h"
 
 namespace Ui {
 class TrainWidget;
@@ -11,11 +15,24 @@ class TrainWidget : public QWidget
 {
     Q_OBJECT
 
+    using kcomp = kanji_data::kanji_compound;
+
 public:
     explicit TrainWidget(QWidget *parent = nullptr);
     ~TrainWidget();
 
+public slots:
+    void setTrainKanji(std::vector<kcomp> newTraining);
+
 private:
+    void setupLayout();
+    void setupButtons();
+    void setupTrainPage();
+
+    QGridLayout *l;
+
+    std::vector<kcomp> trainKanji;
+
     Ui::TrainWidget *ui;
 };
 
